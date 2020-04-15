761950462741b9ea872011ecea22c2ee4c681a1c
Nils Hollmer <n.hollmer@gmail.com>
2020-04-15 14:20:55 +0200

    Update play view for guessing game

--- view/guess/play.php
+++ view/guess/play.php
@@ -11,5 +11,22 @@ namespace Anax\View;
 
 
 ?>
-<h1>Guessing game!</h1>
-<p>Content: <?= $content ?></p>
+<h1>Guess the Number</h1>
+<p>The number is between 1 and 100. You have <?= $tries ?> tries left.</p>
+<form class="" action="handle_post" method="post">
+    <?php if ($tries) { ?>
+        <label for="guess">Your guess: </label>
+        <input type="text" name="guess" value="">
+        <input type="submit" name="doGuess" value="Guess">
+        <input type="submit" name="doCheat" value="Cheat">
+    <?php } ?>
+    <input type="submit" name="doInit" value="Reset">
+</form>
+
+<?php if ($msg) {?>
+    <h2><?= $msg?></h2>
+<?php }; ?>
+
+<?php if ($answer) {?>
+    <h2>The number is <?= $answer?></h2>
+<?php }; ?>
