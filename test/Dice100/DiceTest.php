1e887066b1e40780d091c6da63bee44347003b20
Nils Hollmer <n.hollmer@gmail.com>
2020-04-22 18:22:36 +0200

    Create test directory for Dice100 and create tests for Dice class

---
+++ test/Dice100/DiceTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Nihl\Dice100;
+
+use PHPUnit\Framework\TestCase;
+
+
+/**
+ * Test cases for dice creation, dice roll and graphic output
+ */
+class DiceTest extends TestCase
+{
+    /**
+     * Construct object and verify instance of object
+     */
+    public function testCreateDice()
+    {
+        $dice = new Dice();
+        $this->assertInstanceOf("Nihl\Dice100\Dice", $dice);
+    }
+
+
+    /**
+     * Construct object and verify that value of roll is integer between 1-6
+     */
+    public function testDiceRoll()
+    {
+        $min = 1;
+        $max = 6;
+
+        $dice = new Dice();
+        $roll = $dice->roll();
+
+        $this->assertGreaterThanOrEqual($min, $roll);
+        $this->assertLessThanOrEqual($max, $roll);
+    }
+
+
+    /**
+     * Construct object, roll dice and verify that graphic outputs string in
+     * the following format: "dice-*"
+     */
+    public function testDiceGraphic()
+    {
+        $dice = new Dice();
+        $roll = $dice->roll();
+        $res = $dice->graphic();
+        $exp = "dice-" . $roll;
+
+        $this->assertEquals($exp, $res);
+    }
+}
