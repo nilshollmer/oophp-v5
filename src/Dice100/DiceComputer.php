601ddc0a5cd7b19a6bddef28af96132281d66816
Nils Hollmer <n.hollmer@gmail.com>
2020-04-23 16:58:34 +0200

    Add functionality to DiceComputer

--- src/Dice100/DiceComputer.php
+++ src/Dice100/DiceComputer.php
@@ -1,3 +1,35 @@
 <?php
 
 namespace Nihl\Dice100;
+
+/**
+ * Class for DiceComputer, extends DicePlayer
+ */
+
+class DiceComputer extends DicePlayer
+{
+    /**
+     * @var BASENAME    basename of computer
+     */
+    private const BASENAME = "Computer";
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->name = self::BASENAME . rand(1000, 9999);
+        $this->totalPoints = 0;
+    }
+
+    /**
+     * Checks value of hand against total points and calculates whether to
+     * roll och hold
+     *
+     * @return Boolean
+     */
+    public function calculateMove() : boolean
+    {
+        // if $this->getTotalPoints() >
+    }
+}
