7cd2edfcd6dd13c74a8c07d90f0983f8a3af8783
Nils Hollmer <n.hollmer@gmail.com>
2020-04-25 12:01:10 +0200

    Add testcases for getType and setTotalPoints

--- test/Dice100/DicePlayerTest.php
+++ test/Dice100/DicePlayerTest.php
@@ -14,14 +14,14 @@ class DicePlayerTest extends TestCase
      */
     public function testCreateDicePlayerWithoutArgument()
     {
-        $dp = new DicePlayer();
-        $this->assertInstanceOf("Nihl\Dice100\DicePlayer", $dp);
+        $diceplayer = new DicePlayer();
+        $this->assertInstanceOf("Nihl\Dice100\DicePlayer", $diceplayer);
 
         $exp = '/(Player)\d{4}/';
-        $this->assertMatchesRegularExpression($exp, $dp->getName());
+        $this->assertMatchesRegularExpression($exp, $diceplayer->getName());
 
         $exp = 0;
-        $this->assertEquals($exp, $dp->getTotalPoints());
+        $this->assertEquals($exp, $diceplayer->getTotalPoints());
     }
 
     /**
@@ -40,19 +40,19 @@ class DicePlayerTest extends TestCase
      */
     public function testDicePlayerAddPoints()
     {
-        $dp = new DicePlayer();
+        $diceplayer = new DicePlayer();
 
         $points = 10;
         $exp = 10;
 
-        $dp->addPoints($points);
-        $this->assertEquals($exp, $dp->getTotalPoints());
+        $diceplayer->addPoints($points);
+        $this->assertEquals($exp, $diceplayer->getTotalPoints());
 
         $points = 100;
         $exp = 110;
 
-        $dp->addPoints($points);
-        $this->assertEquals($exp, $dp->getTotalPoints());
+        $diceplayer->addPoints($points);
+        $this->assertEquals($exp, $diceplayer->getTotalPoints());
     }
 
     /**
@@ -60,16 +60,52 @@ class DicePlayerTest extends TestCase
      */
     public function testDicePlayerHasWon()
     {
-        $dp = new DicePlayer();
+        $diceplayer = new DicePlayer();
 
         $points = 10;
 
-        $dp->addPoints($points);
-        $this->assertFalse($dp->hasWon());
+        $diceplayer->addPoints($points);
+        $this->assertFalse($diceplayer->hasWon());
 
         $points = 100;
 
-        $dp->addPoints($points);
-        $this->assertTrue($dp->hasWon());
+        $diceplayer->addPoints($points);
+        $this->assertTrue($diceplayer->hasWon());
+    }
+
+    /**
+     * Construct object and get its type
+     */
+     public function testDicePlayerGetType()
+     {
+         $diceplayer = new DicePlayer();
+         $exp = "Player";
+         $res = $diceplayer->getType();
+
+         $this->assertEquals($exp, $res);
+     }
+
+    /**
+     *
+     */
+    public function testDicePlayerSetTotalPoints()
+    {
+        $diceplayer = new DicePlayer();
+
+        $exp = -1;
+        $diceplayer->setTotalPoints(-1);
+        $this->assertEquals($exp, $diceplayer->getTotalPoints());
+
+        $exp = 0;
+        $diceplayer->setTotalPoints(0);
+        $this->assertEquals($exp, $diceplayer->getTotalPoints());
+
+        $exp = 20;
+        $diceplayer->setTotalPoints(20);
+        $this->assertEquals($exp, $diceplayer->getTotalPoints());
+
+        $exp = 100;
+        $diceplayer->setTotalPoints(100);
+        $this->assertEquals($exp, $diceplayer->getTotalPoints());
     }
 }
