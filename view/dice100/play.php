813a4cd0c281485576a922cbe5a5d9ced78ebcb7
Nils Hollmer <n.hollmer@gmail.com>
2020-04-25 11:30:42 +0200

    Create gameplay view

--- view/dice100/play.php
+++ view/dice100/play.php
@@ -12,3 +12,38 @@ namespace Anax\View;
 
 ?>
 <h1>Dice 100</h1>
+<h3>Scoreboard</h3>
+<?php foreach ($game->getPlayers() as $player) { ?>
+    <p><?= $player->getName()?> : <?= $player->getTotalPoints()?></p>
+<?php } ?>
+
+<div style="background-color: #eee;">
+    <?php if (!$gamestate["hasWon"]) { ?>
+        <h3><?= $gamestate["active"]->getName() ?>s Turn:</h3>
+
+        <form class="" action="action" method="post">
+
+            <?php if ($gamestate["turnIsOver"]) { ?>
+                <input type="submit" name="action" value="Next Turn">
+
+            <?php } elseif ($gamestate["active"]->getType() == "Computer") { ?>
+                <input type="submit" name="action" value="Pass">
+
+            <?php } elseif ($gamestate["active"]->getType() == "Player") {?>
+                <input type="submit" name="action" value="Roll">
+                <input type="submit" name="action" value="Hold">
+
+            <?php } ?>
+        </form>
+
+        <p>Points: <?= $gamestate["currentPoints"] ?></p>
+        <p class="dice-utf8">
+            Rolls: <?= $gamestate["diceHand"] ?>
+        </p>
+
+    <?php } else {?>
+        <h3><?= $gamestate["hasWon"]->getName() ?> HAS WON! </h3>
+        <a href="/">New game</a>
+
+    <?php }?>
+</div>
