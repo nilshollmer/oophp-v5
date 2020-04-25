ba60b4bf1c4cfdb79533da468227fc28a744e37d
Nils Hollmer <n.hollmer@gmail.com>
2020-04-25 11:52:09 +0200

    Add testcases for calculateMove()

--- test/Dice100/DiceComputerTest.php
+++ test/Dice100/DiceComputerTest.php
@@ -12,18 +12,70 @@ class DiceComputerTest extends TestCase
 
 
     /**
-     * Construct objects with no argument and verify its properties
+     * Construct object and verify its properties
      */
     public function testCreateDiceComputer()
     {
-        $dc= new DiceComputer();
-        $this->assertInstanceOf("Nihl\Dice100\DiceComputer", $dc);
+        $dicecomputer= new DiceComputer();
+        $this->assertInstanceOf("Nihl\Dice100\DiceComputer", $dicecomputer);
 
         $exp = '/(Computer)\d{4}/';
-        $this->assertMatchesRegularExpression($exp, $dc->getName());
+        $this->assertMatchesRegularExpression($exp, $dicecomputer->getName());
 
         $exp = 0;
-        $this->assertEquals($exp, $dc->getTotalPoints());
+        $this->assertEquals($exp, $dicecomputer->getTotalPoints());
     }
 
+    /**
+     * Construct object and verify that calculateMove with 0 as argument
+     * return true
+     */
+    public function testCalculateMoveArgumentEqualsZero()
+    {
+        $dicecomputer= new DiceComputer();
+        $arg = 0;
+        $move = $dicecomputer->calculateMove($arg);
+
+        $this->assertTrue($move);
+    }
+
+    /**
+     * Construct object and verify that calculateMove with 20 as argument
+     * return false
+     */
+    public function testCalculateMoveArgumentEqualsTwenty()
+    {
+        $dicecomputer= new DiceComputer();
+        $arg = 20;
+        $move = $dicecomputer->calculateMove($arg);
+
+        $this->assertFalse($move);
+    }
+
+    /**
+     * Construct object and verify that calculateMove  with 100 as argument
+     * return false
+     */
+    public function testCalculateMoveArgumentEqualsOneHundred()
+    {
+        $dicecomputer= new DiceComputer();
+        $arg = 100;
+        $move = $dicecomputer->calculateMove($arg);
+
+        $this->assertFalse($move);
+    }
+
+    /**
+     * Construct object, add 99 points and verify that calculateMove with 1 as argument
+     * return false
+     */
+    public function testAddPointsCalculateMoveArgumentEqualsOne()
+    {
+        $dicecomputer= new DiceComputer();
+        $dicecomputer->addPoints(99);
+        $arg = 1;
+        $move = $dicecomputer->calculateMove($arg);
+
+        $this->assertFalse($move);
+    }
 }
