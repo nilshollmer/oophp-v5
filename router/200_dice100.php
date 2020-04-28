f4e921b13d946b6ca1e166efb31dc9551a323bc1
Nils Hollmer <n.hollmer@gmail.com>
2020-04-28 10:12:06 +0200

    Remove  syntax for framework session

--- router/200_dice100.php
+++ router/200_dice100.php
@@ -27,11 +27,15 @@ $app->router->get("dice100/init", function () use ($app) {
     $numComp = isset($_GET["numComp"]) ? $_GET["numComp"] : 1;
     $dice = isset($_GET["dice"]) ? $_GET["dice"] : 2;
 
-    unset($_SESSION["game"]);
-    $_SESSION["game"] = new Nihl\Dice100\Dice100();
-    $_SESSION["diceHand"] = new Nihl\Dice100\DiceHand($dice);
-    $_SESSION["game"]->createPlayers($name, $numComp);
-    $_SESSION["game"]->initGamestate();
+    $app->session->delete("game");
+
+    $app->session->set("game", new Nihl\Dice100\Dice100());
+
+    $app->session->set("diceHand", new Nihl\Dice100\DiceHand($dice));
+
+    $app->session->get("game")->createPlayers($name, $numComp);
+
+    $app->session->get("game")->initGamestate();
 
     return $app->response->redirect("dice100/play");
 });
@@ -42,12 +46,13 @@ $app->router->get("dice100/init", function () use ($app) {
 $app->router->get("dice100/play", function () use ($app) {
     // Play the game
     $title = "Dice100 | Play";
-    $game = $_SESSION["game"];
+    $game = $app->session->get("game");
+
 
     $data = [
         "game" => $game,
         "gamestate" => $game->getGamestate(),
-        "diceHand" => $_SESSION["diceHand"]
+        "diceHand" => $app->session->get("diceHand")
     ];
 
     $app->page->add("dice100/play", $data);
@@ -64,26 +69,60 @@ $app->router->get("dice100/play", function () use ($app) {
 $app->router->post("dice100/action", function () use ($app) {
     switch ($_POST["action"]) {
         case "Roll":
-            $_SESSION["diceHand"]->rollDice();
-            $_SESSION["game"]->checkHand($_SESSION["diceHand"]);
+            return $app->response->redirect("dice100/roll");
             break;
         case "Hold":
-            $_SESSION["game"]->holdHand();
+            return $app->response->redirect("dice100/hold");
             break;
         case "Next Turn":
-            $_SESSION["game"]->startNextTurn();
+            return $app->response->redirect("dice100/nextturn");
             break;
         case "Pass":
-            if ($_SESSION["game"]->computerMove()) {
-                $_SESSION["diceHand"]->rollDice();
-                $_SESSION["game"]->checkHand($_SESSION["diceHand"]);
-                break;
-            }
-            $_SESSION["game"]->holdHand();
+            return $app->response->redirect("dice100/pass");
             break;
     }
-    // $_SESSION["game"]->runGame($_POST["action"]);
 
+    return $app->response->redirect("dice100/play");
+});
+
+/**
+ * Handle Roll action
+ */
+$app->router->get("dice100/roll", function () use ($app) {
+    $app->session->get("diceHand")->rollDice();
+    $app->session->get("game")->checkHand($app->session->get("diceHand"));
+
+    return $app->response->redirect("dice100/play");
+});
+
+/**
+ * Handle Hold action
+ */
+$app->router->get("dice100/hold", function () use ($app) {
+    $app->session->get("game")->holdHand();
+
+    return $app->response->redirect("dice100/play");
+});
+
+/**
+ * Handle Next Turn action
+ */
+$app->router->get("dice100/nextturn", function () use ($app) {
+    $app->session->get("game")->startNextTurn();
+
+    return $app->response->redirect("dice100/play");
+});
+
+/**
+ * Handle Pass action
+ */
+$app->router->get("dice100/pass", function () use ($app) {
+    if ($app->session->get("game")->computerMove()) {
+        $app->session->get("diceHand")->rollDice();
+        $app->session->get("game")->checkHand($app->session->get("diceHand"));
+    } else {
+        $app->session->get("game")->holdHand();
+    }
     return $app->response->redirect("dice100/play");
 });
 
