4b47854537c8be1f1e8c0f94570eb3e5cbd580cc
Nils Hollmer <n.hollmer@gmail.com>
2020-05-01 12:49:16 +0200

    Add functionality to Histogram

--- src/Dice100/Histogram.php
+++ src/Dice100/Histogram.php
@@ -10,10 +10,24 @@ class Histogram
 {
     /**
      * @var array   $serie      Numbers stored in sequence
+     * @var int     $min        The lowest possible number
+     * @var int     $max        The highest possible number
      */
     private $serie = [];
+    private $rolls = [];
+    private $min = 1;
+    private $max = 6;
 
-
+    /**
+     * Create the object and initialize rolls array to an array of Zeros
+     *
+     */
+    public function __construct()
+    {
+        foreach (range($this->min, $this->max) as $key) {
+            $this->rolls[$key] = "";
+        }
+    }
 
     /**
      * Get the serie
@@ -35,20 +49,44 @@ class Histogram
      */
     public function injectData(HistogramInterface $object)
     {
-        $this->serie    =   $object->getHistogramSerie();
-        $this->min      =   $object->getHistogramMin();
-        $this->max      =   $object->getHistogramMax();
+        $serie = $object->getHistogramSerie();
+        foreach ($serie as $key) {
+            $this->rolls[$key] .= "*";
+        }
+        array_unshift($this->serie, $serie);
     }
 
 
+    /**
+     * Get statistics of rolls from the game
+     *
+     * @return string representing the histogram.
+     */
+    public function getStatistics()
+    {
+        $output = "";
+
+        foreach (array_keys($this->rolls) as $key) {
+            $output .= "<p>" . $key . ": " . $this->rolls[$key] . "</p>";
+        }
+
+        return $output;
+    }
 
     /**
-     * Get histogram as text
+     * Get the last individual rolls
      *
      * @return string representing the histogram.
      */
     public function getAsText()
     {
-        return "Histogramserie";
+        $output = "";
+        $sizeOfOutput = 6;
+
+        foreach (array_slice($this->serie, 0, $sizeOfOutput) as $hand) {
+            $output .= "<p>" . implode(", ", $hand) . "</p>";
+        }
+
+        return $output;
     }
 }
