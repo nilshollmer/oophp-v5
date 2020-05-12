ac221821b5619689472311d2022d2a4d15ae91ae
Nils Hollmer <n.hollmer@gmail.com>
2020-05-12 07:30:32 +0200

    Add router for movie database

---
+++ router/300_movie.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Routes
+ */
+
+ /**
+  * Show all movies.
+  */
+$app->router->get("movie", function () use ($app) {
+    $title = "Movie database | oophp";
+
+    $app->db->connect();
+    $sql = "SELECT * FROM movie;";
+    $res = $app->db->executeFetchAll($sql);
+
+    $app->page->add("movie/index", [
+        "resultset" => $res,
+    ]);
+
+    return $app->page->render([
+        "title" => $title,
+    ]);
+});
