ed73986d70a5e2372c05c0493793630edd5af857
Nils Hollmer <n.hollmer@gmail.com>
2020-05-12 10:21:47 +0200

    Instansiate Movie Controller

---
+++ config/router/200_movie.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Dice controller
+ */
+return [
+    "routes" => [
+        [
+            "info" => "Movie Controller.",
+            "mount" => "movie",
+            "handler" => "\Nihl\Movie\MovieController",
+        ],
+    ]
+];
