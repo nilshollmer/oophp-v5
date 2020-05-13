08912bae6143b0d70d5938a08103b4fa06d0c427
Nils Hollmer <n.hollmer@gmail.com>
2020-05-13 17:20:06 +0200

    Add report links to header

--- config/navbar/header.php
+++ config/navbar/header.php
@@ -40,6 +40,21 @@ return [
                         "url" => "redovisning/kmom04",
                         "title" => "Redovisning för kmom04.",
                     ],
+                    [
+                        "text" => "Kmom05",
+                        "url" => "redovisning/kmom05",
+                        "title" => "Redovisning för kmom05.",
+                    ],
+                    [
+                        "text" => "Kmom06",
+                        "url" => "redovisning/kmom06",
+                        "title" => "Redovisning för kmom06.",
+                    ],
+                    [
+                        "text" => "Kmom10",
+                        "url" => "redovisning/kmom10",
+                        "title" => "Redovisning för kmom10.",
+                    ],
                 ],
             ],
         ],
