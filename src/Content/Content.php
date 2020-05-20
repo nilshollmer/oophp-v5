aa4311a9554587b4ca11247bc643412a992fd86c
Nils Hollmer <n.hollmer@gmail.com>
2020-05-20 13:19:40 +0200

    Update controller

--- src/Content/Content.php
+++ src/Content/Content.php
@@ -30,6 +30,198 @@ class Content
     public $updated;
     public $deleted;
 
+
+
+    /**
+     * Fetch all entries in $db
+     *
+     * @param Database  $db     Database to fetch from
+     *
+     * @return resultset
+     */
+    public static function contentFetchAll($db) {
+        $sql = "SELECT * FROM content;";
+        return $db->executeFetchAll($sql);
+    }
+
+
+
+    /**
+     * Create new content and return its id
+     *
+     * @param Database  $db     Database to fetch from
+     * @param String    $title  Title of new content
+     *
+     * @return integer id of last inserted content
+     */
+    public static function contentCreate($db, $title) {
+        $sql = "INSERT INTO content (title) VALUES (?);";
+        $db->execute($sql, [$title]);
+
+        return $db->lastInsertId();
+    }
+
+
+
+    /**
+     * Get content using id
+     *
+     * @param Database  $db     Database to fetch from
+     * @param Integer   $id     Id of content to fetch
+     *
+     * @return resultset
+     */
+    public static function contentFetch($db, $id) {
+        $sql = "SELECT * FROM content WHERE id = ?;";
+        return $db->executeFetch($sql, [$id]);
+    }
+
+
+
+    /**
+     *  Update content
+     *
+     * @param Database  $db     Database to fetch from
+     * @param Array     $params Parameters to update with
+     *
+     * @return resultset
+     */
+    public static function contentUpdate($db, $params) {
+        if (!$params["contentSlug"]) {
+            $params["contentSlug"] = slugify($params["contentTitle"]);
+        }
+
+        if (!$params["contentPath"]) {
+                $params["contentPath"] = null;
+        }
+
+        $sql = "UPDATE content SET title=?, path=?, slug=?, data=?, type=?, filter=?, published=? WHERE id = ?;";
+
+        try {
+            $db->execute($sql, array_values($params));
+        } catch (Exception $e) {
+            var_dump("hej");
+        }
+    }
+
+
+
+    /**
+     * Set content with id to deleted
+     *
+     * @param Database  $db     Database to fetch from
+     * @param Array     $id     Content to delete
+     *
+     * @return resultset
+     */
+    public static function contentDelete($db, $id) {
+        $sql = "UPDATE content SET deleted=NOW() WHERE id = ?;";
+        $db->execute($sql, [$id]);
+    }
+
+
+    /**
+     * Fetch all content with type of page
+     *
+     * @param Database  $db     Database to fetch from
+     *
+     * @return resultset
+     */
+    public static function contentFetchAllPages($db) {
+        $sql = <<<EOD
+SELECT
+    *,
+    CASE
+        WHEN (deleted <= NOW()) THEN "isDeleted"
+        WHEN (published <= NOW()) THEN "isPublished"
+        ELSE "notPublished"
+    END AS status
+FROM content
+WHERE type=?
+;
+EOD;
+        return $db->executeFetchAll($sql, ["page"]);
+    }
+
+
+
+    /**
+     * Fetch all content with type of post
+     *
+     * @param Database  $db     Database to fetch from
+     *
+     * @return resultset
+     */
+    public static function contentFetchAllPosts($db) {
+        $sql = <<<EOD
+SELECT
+    *,
+    DATE_FORMAT(COALESCE(updated, published), '%Y-%m-%dT%TZ') AS published_iso8601,
+    DATE_FORMAT(COALESCE(updated, published), '%Y-%m-%d') AS published
+FROM content
+WHERE type=?
+ORDER BY published DESC
+;
+EOD;
+        return $db->executeFetchAll($sql, ["post"]);
+    }
+
+
+
+    /**
+     * Fetch blogpost using slug
+     *
+     * @param Database  $db     Database to fetch from
+     * @param String    $slug   Slug to content to fetch
+     *
+     * @return resultset
+     */
+    public static function contentFetchBlogPost($db, $slug) {
+        $sql = <<<EOD
+SELECT
+    *,
+    DATE_FORMAT(COALESCE(updated, published), '%Y-%m-%dT%TZ') AS published_iso8601,
+    DATE_FORMAT(COALESCE(updated, published), '%Y-%m-%d') AS published
+FROM content
+WHERE
+    slug = ?
+    AND type = ?
+    AND (deleted IS NULL OR deleted > NOW())
+    AND published <= NOW()
+ORDER BY published DESC
+;
+EOD;
+        return $db->executeFetch($sql, [$slug, "post"]);
+    }
+
+    /**
+     * Fetch page using path
+     *
+     * @param Database  $db     Database to fetch from
+     * @param String    $path   Path to content to fetch
+     *
+     * @return resultset
+     */
+    public static function contentFetchPage($db, $path) {
+        $sql = <<<EOD
+SELECT
+    *,
+    DATE_FORMAT(COALESCE(updated, published), '%Y-%m-%dT%TZ') AS modified_iso8601,
+    DATE_FORMAT(COALESCE(updated, published), '%Y-%m-%d') AS modified
+FROM content
+WHERE
+    path = ?
+    AND type = ?
+    AND (deleted IS NULL OR deleted > NOW())
+    AND published <= NOW()
+;
+EOD;
+        return $db->executeFetch($sql, [$path, "page"]);
+    }
+
+
+
+
     /**
      * Set
      */
@@ -37,8 +229,10 @@ class Content
         $this[$name] = $value;
     }
 
+
+
     /**
-     * posix_getgrgid
+     * Get
      */
     public function __get($name) {
         return $this[$name];
--- src/Content/ContentController.php
+++ src/Content/ContentController.php
@@ -23,8 +23,10 @@ class ContentController implements AppInjectableInterface
 
     /**
      * @var string $db
+     * @var string $errormessage
      */
     private $db = "not active";
+    private $errormessage = "";
 
 
 
@@ -40,8 +42,8 @@ class ContentController implements AppInjectableInterface
         // Use to initialise member variables.
         $this->db = "active";
         $this->app->db->connect();
-        // $this->app->page->add("movie/movie-header", [], "main");
-        // $this->app->page->add("movie/movie-nav", [], "main");
+        $this->app->page->add("cms/header", [], "main");
+        $this->app->page->add("cms/navbar", [], "sidebar-left");
     }
 
 
@@ -52,21 +54,251 @@ class ContentController implements AppInjectableInterface
      * ANY METHOD mountpoint/
      * ANY METHOD mountpoint/index
      *
-     * @return string
+     * @return object
      */
     public function indexAction() : object
     {
         // Deal with the action and return a response.
         $title = "Content";
 
-        $sql = "SELECT * FROM content WHERE id = 1;";
-        $obj = new Content();
-        $res = $this->app->db->executeFetchInto($sql, [], $obj);
+        // $sql = "SELECT * FROM content;";
         // $res = $this->app->db->executeFetchAll($sql);
+
+        // $obj = new Content();
+        // $res = $this->app->db->executeFetchInto($sql, [], $obj);
         // var_dump($res);
+        $res = Content::contentFetchAll($this->app->db);
+
         $this->app->page->add("cms/index", [ "res" => $res ]);
 
         return $this->app->page->render([ "title" => $title ]);
     }
 
+
+    /**
+     * This is the admin method action, it handles:
+     * ANY METHOD mountpoint/admin
+     *
+     * @return object
+     */
+    public function adminAction() : object
+    {
+        // Deal with the action and return a response.
+        $title = "Admin";
+        //
+        // $sql = "SELECT * FROM content;";
+        // $res = $this->app->db->executeFetchAll($sql);
+        $res = Content::contentFetchAll($this->app->db);
+        $this->app->page->add("cms/admin", [ "res" => $res ]);
+
+        return $this->app->page->render([ "title" => $title ]);
+    }
+
+    /**
+     * This is the create method action, it handles:
+     * ANY METHOD mountpoint/admin
+     *
+     * @return object
+     */
+    public function createAction() : object
+    {
+        // Deal with the action and return a response.
+        $title = "Create";
+        // $sql = "SELECT * FROM content;";
+        // $res = $this->app->db->executeFetchAll($sql);
+        // $res = Content::contentFetchAll($this->app->db);
+        $this->app->page->add("cms/create", []);
+
+        return $this->app->page->render([ "title" => $title ]);
+    }
+
+    /**
+     * This is the create method action for Post, it handles:
+     * ANY METHOD mountpoint/admin
+     *
+     * @return object
+     */
+    public function createActionPost() : object
+    {
+        // Deal with the action and return a response.
+
+        $title = $this->app->request->getPost("title", null);
+        if ($title) {
+            $id = Content::contentCreate($this->app->db, $title);
+            return $this->app->response->redirect("content/edit/{$id}");
+        }
+        return $this->app->response->redirect("content");
+    }
+
+    /**
+     * This is the edit method action, it handles:
+     * ANY METHOD mountpoint/edit/<id>
+     *
+     * @param integer $id of content to edit
+     *
+     * @return object
+     */
+    public function editAction($id) : object
+    {
+        // Deal with the action and return a response.
+        $title = "Create";
+
+        if (!is_numeric($id)) {
+            die("Not valid for content id.");
+        }
+
+        $content = Content::contentFetch($this->app->db, $id);
+
+        $this->app->page->add("cms/edit", [ "content" => $content ]);
+
+        return $this->app->page->render([ "title" => $title ]);
+    }
+
+
+    /**
+     * This is the edit method action for Post, it handles:
+     * ANY METHOD mountpoint/edit
+     *
+     * @return object
+     */
+    public function editActionPost() : object
+    {
+        // Deal with the action and return a response.
+
+        if (hasKeyPost("doSave")) {
+            $params = getPostArray([
+                "contentTitle",
+                "contentPath",
+                "contentSlug",
+                "contentData",
+                "contentType",
+                "contentFilter",
+                "contentPublish",
+                "contentId"
+            ]);
+
+            try {
+                Content::contentUpdate($this->app->db, $params);
+            } catch (Exception $e){
+                $this->errormessage = $e;
+                return $this->app->response->redirect("content/error");
+            }
+
+            return $this->app->response->redirect("content/edit/" . $params["contentId"]);
+        }
+
+
+        if (hasKeyPost("doDelete")) {
+            return $this->app->response->redirect("content/delete/" . getPostArray("contentId"));
+        }
+
+        return $this->app->response->redirect("content");
+    }
+
+    /**
+     * This is the delete method action, it handles:
+     * ANY METHOD mountpoint/delete/<id>
+     *
+     * @param integer $id of content to delete
+     *
+     * @return object
+     */
+    public function deleteAction($id) : object
+    {
+        // Deal with the action and return a response.
+        $title = "Delete";
+
+        if (!is_numeric($id)) {
+            die("Not valid for content id.");
+        }
+
+        $content = Content::contentFetch($this->app->db, $id);
+
+        $this->app->page->add("cms/delete", [ "content" => $content ]);
+
+        return $this->app->page->render([ "title" => $title ]);
+    }
+
+    /**
+     * This is the delete method action with POST, it handles:
+     * ANY METHOD mountpoint/delete
+     *
+     *
+     * @return object
+     */
+    public function deleteActionPost() : object
+    {
+        // Deal with the action and return a response.
+        $id = $this->app->request->getPost("contentId");
+        if ($id) {
+            Content::contentDelete($this->app->db, $id);
+        }
+
+        return $this->app->response->redirect("content/admin");
+    }
+
+
+    /**
+     * Handles pages method
+     * mountpoint/pages
+     * mountpoint/pages/<path>
+     *
+     * @return object
+     */
+    public function pagesAction($path = null) : object
+    {
+        if ($path) {
+            $content = Content::contentFetchPage($this->app->db, $path);
+            $title = $content->title;
+            $this->app->page->add("cms/page", [ "content" => $content ]);
+            return $this->app->page->render([ "title" => $title ]);
+        }
+
+        $title = "View pages";
+        $res = Content::contentFetchAllPages($this->app->db);
+        $this->app->page->add("cms/pages", [ "res" => $res ]);
+        return $this->app->page->render([ "title" => $title ]);
+    }
+
+
+
+    /**
+     * Handles blog and blogpost method
+     * mountpoint/blog
+     * mountpoint/blog/<slug>
+     *
+     * @return object
+     */
+    public function blogAction($slug = null) : object
+    {
+        if ($slug) {
+            $content = Content::contentFetchBlogPost($this->app->db, $slug);
+            $title = $content->title;
+            $this->app->page->add("cms/blogpost", [ "content" => $content ]);
+            return $this->app->page->render([ "title" => $title ]);
+        }
+
+        $title = "View blog";
+        $res = Content::contentFetchAllPosts($this->app->db);
+        $this->app->page->add("cms/blog", [ "res" => $res ]);
+        return $this->app->page->render([ "title" => $title ]);
+    }
+
+
+
+    /**
+     * This is the Error page
+     * ANY METHOD mountpoint/error
+     *
+     *
+     * @return object
+     */
+    public function errorAction() : object
+    {
+        $title = "Error";
+
+        $this->app->page->add("cms/error", [ "error" => $this->errormessage ]);
+
+        return $this->app->page->render([ "title" => $title ]);
+    }
 }
